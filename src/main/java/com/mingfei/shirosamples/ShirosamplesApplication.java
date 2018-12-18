dd93fb177fbb43bbe223dedc0ab0efd2568c33a5
mfc <mf.c@foxmail.com>
2018-12-18 21:03:10 +0800

    Initial commit

---
+++ src/main/java/com/mingfei/shirosamples/ShirosamplesApplication.java
@@ -0,0 +1,14 @@
+package com.mingfei.shirosamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShirosamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShirosamplesApplication.class, args);
+    }
+
+}
+
